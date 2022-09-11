e8e2ca190fcd52b5dd7e6b47fb075fe62bb41569
Ivan Perez <ivan.web9450@gmail.com>
2022-09-10 21:39:51 -0400

    question2 | ky8

---
+++ JS/script.js
@@ -0,0 +1,17 @@
+// Question1 | ky8
+// While making a game, your partner, Greg, decided to create a function to check if the user is still alive called checkAlive/CheckAlive/check_alive. Unfortunately, Greg made some errors while creating the function. checkAlive/CheckAlive/check_alive should return true if the player's health is greater than 0 or false if it is 0 or below. The function receives one parameter health which will always be a whole number between -10 and 10.
+function checkAlive(health) {
+  return health > 0;
+}
+
+// Question2 | ky8
+// In this kata you need to check the provided array (x) for good ideas 'good' and bad ideas 'bad'. If there are one or two good ideas, return 'Publish!', if there are more than 2 return 'I smell a series!'. If there are no good ideas, as is often the case, return 'Fail!'.
+
+const well = (x) => {
+  const good_count = x.filter((x) => x == "good").length;
+  return good_count < 1
+    ? "Fail!"
+    : good_count < 3
+    ? "Publish!"
+    : "I smell a series!";
+};
