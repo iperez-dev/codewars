a7c641886d63d09b08326c6836fdd5e8a02ae4fa
Ivan Perez <ivan.web9450@gmail.com>
2022-09-18 11:09:43 -0400

    8/18/2022

--- JS/script.js
+++ JS/script.js
@@ -57,3 +57,19 @@ function fiveLine(s) {
 function noSpace(x) {
   return x.split(" ").join("");
 }
+
+//-----------------------------------------------------------------------------------------------------------------------
+
+//Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
+// for loop
+
+var summation = function (num) {
+  let result = 0;
+  for (var i = 1; i <= num; i++) {
+    result += i;
+  }
+
+  return result;
+};
+
+//-----------------------------------------------------------------------------------------------------------------------
