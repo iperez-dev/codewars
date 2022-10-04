50cc035252d8f030c9d73444a3163b95efbc335d
Ivan Perez <ivan.web9450@gmail.com>
2022-10-04 11:14:15 -0400

    10-4-2022

--- JS/script.js
+++ JS/script.js
@@ -84,7 +84,7 @@ function betterThanAverage(classPoints, yourPoints) {
 
 //-----------------------------------------------------------------------------------------------------------------------
 
-const point=games=>games.reduce((output,current)=>{
+const points=games=>games.reduce((output,current)=>{
     return output += current[0]>current[2] ? 3 : current[0]===current[2] ? 1 : 0;
   },0)
 
@@ -305,7 +305,7 @@ function solution(a, b){
 // .charAt(0) method, at index 0 select the specified character of the string. / the first one in this case
 
 function areYouPlayingBanjo(name) {
-  return name.charAt(0)
+  return name.charAt(0
 }
 
 //-----------------------------------------------------------------------------------------------------------------------
@@ -334,9 +334,30 @@ padEnd()
 // .toArray() method is used to convert into a normal array
 
 
-
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//
+// .substring() method is used to convert into a normal array
+
+
+const str = 'Mozilla';
+
+console.log(str.substring(1, 3));
+// expected output: "oz"
+
+console.log(str.substring(2));
+// expected output: "zilla"
+
+//-----------------------------------------------------------------------------------------------------------------------
+
+
+function pigIt(str){
+  //Code here
+  return str.split(' ').map(word => {
+    return word.substring(1) + word[0] + 'ay';
+  }).join(' ');
+}
+
+
+
 
 
 
@@ -493,6 +514,15 @@ function updateLight(current) {
 
 //-----------------------------------------------------------------------------------------------------------------------
 
+function finalGrade (exam, projects) {
+  return exam > 90 || projects > 10 ? 100 : exam > 75 && projects >= 5 ? 90 : exam > 50 && projects >= 2 ? 75 : 0;
+}
+
+//-----------------------------------------------------------------------------------------------------------------------
+
+
+
+
 
 
 
@@ -628,3 +658,43 @@ function oddCount(n){
   return Math.floor(n/2);
 }
 
+
+// -----------------------------------------------------------------------------------------------------------------------
+// 14 - Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
+
+function pigIt(str){
+  //Code here
+  return str.split(' ').map(word => {
+    return word.substring(1) + word[0] + 'ay';
+  }).join(' ');
+}
+
+
+
+
+
+
+
+Tech Questions/cold message template
+
+"I'm a freelance web developer who's interested in [whatever things they do that interests me] and I'm really inspired how you [whatever I could cobble together from their LinkedIn resume] and how you got your role in the company. Do you have 15 minutes to chat with me about x, and y?" And then ahead of the meeting, I research them and the company they work for, write down a bunch of questions and go from there.
+
+1. How did you get your foot in the door at _?
+2. What education, experience, or training do you find the most useful in your current role? (i.e. did the stuff you learned in college/self-taught help you most or job experience over time)
+3. How does your job affect your general lifestyle?
+4. What are the typical challenges you face in the tech industry?
+5. Do employers care if you don’t have a degree in CS?
+6. As a __, what steps would you recommend I take to prepare to enter this field?
+7. What tips can you give to make someone stand out, especially in competitive places?
+8. Looking back at your careers so far, are there things you wish you would have done differently?
+9. Can you suggest a few names of people I could contact for additional info?
+10. What is a project you’re excited about working on? 
+11. What is one thing that you have always wanted to try but never have?
+12. What is something that you have always wanted to learn?
+13. What has been the highlight of your year so far? 
+14. “How did you land your first job?”
+15. “What’s your current company like, and what’s your role there?”
+16. “How will I know when I’m ready to start looking for a job?”
+
+Here is a link to one of Leon's past streams, he has some goods in here on networking :meowcoffee: 
+https://www.youtube.com/watch?v=OaglXfjsBaE
