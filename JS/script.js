3610df14861e6b272643f5b14afce41e17374c7e
Ivan Perez <ivan.web9450@gmail.com>
2022-10-11 15:20:39 -0400

    10-11-2022

--- JS/script.js
+++ JS/script.js
@@ -8,7 +8,7 @@
 // .sort()
 //// .map()
 // .concat()
-// .filter()
+//// .filter()
 // .reverse()
 // .replace()
 // .repeat()
@@ -228,8 +228,31 @@ function abbrevName(name) {
 
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// .filter() method creates a new array filled with elements that pass a test provided by a function.
+// .filter() The filter() method creates a shallow copy of a portion of a given array, filtered down to just the elements from the given array that pass the test implemented by the provided function.
+//-----------------------------------------------------------------------------------------------------------------------
 
+const array = [-3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+
+function isPrime(num) {
+  for (let i = 2; num > i; i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return num > 1;
+}
+
+
+console.log(array.filter(isPrime)); // [2, 3, 5, 7, 11, 13]
+//-----------------------------------------------------------------------------------------------------------------------
+const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
+
+const result = words.filter(word => word.length > 6);
+
+console.log(result);
+// expected output: Array ["exuberant", "destruction", "present"]
+
+//-----------------------------------------------------------------------------------------------------------------------
 
 function isMultiple(arr) {
   return arr.filter((e, i) => e % i === 0);
@@ -938,3 +961,6 @@ const phrases = [
 function howMuchILoveYou(n) {
      return phrases[(n - 1) % phrases.length] 
 }
+
+
+
