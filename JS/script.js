83d92140996adc3d3dca5f398749496303fb2cdb
Ivan Perez <ivan.web9450@gmail.com>
2022-10-13 21:53:18 -0400

    10-13-2022

--- JS/script.js
+++ JS/script.js
@@ -956,11 +956,11 @@ function arrayDiff(a, b) {
 "passionately"
 "madly"
 "not at all"
-If there are more than 6 petals, you start over with "I love you" for 7 petals, "a little" for 8 petals and so on.
+// If there are more than 6 petals, you start over with "I love you" for 7 petals, "a little" for 8 petals and so on.
 
-When the last petal was torn there were cries of excitement, dreams, surging thoughts and emotions.
+// When the last petal was torn there were cries of excitement, dreams, surging thoughts and emotions.
 
-Your goal in this kata is to determine which phrase the girls would say at the last petal for a flower of a given number of petals. The number of petals is always greater than 0.
+// Your goal in this kata is to determine which phrase the girls would say at the last petal for a flower of a given number of petals. The number of petals is always greater than 0.
 
 
 const phrases = [
