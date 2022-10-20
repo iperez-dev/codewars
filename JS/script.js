7f25923f5c009e25950c6a4b79ca43a8f46d071e
Ivan Perez <ivan.web9450@gmail.com>
2022-10-20 19:49:24 -0400

    10-20-2022

--- JS/script.js
+++ JS/script.js
@@ -972,20 +972,5 @@ const phrases = [
     'not at all',
 ]
 
-function howMuchILoveYou(n) {
-     return phrases[(n - 1) % phrases.length] 
-}
 
-const phrases = [
-  'I love you',
-  'a little',
-  'a lot',
-  'passionately',
-  'madly',
-  'not at all',
-]
-
-function howMuchILoveYou(n) {
-   return phrases[(n - 1) % phrases.length] 
-}
 
