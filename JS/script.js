d9059acaeb172d673a7918ded08b37d44d034d6a
Ivan Perez <ivan.web9450@gmail.com>
2023-07-05 08:59:18 -0400

    Question16 | ky8 | Grasshopper - Terminal game move function

--- JS/script.js
+++ JS/script.js
@@ -218,3 +218,45 @@ function pigIt(str) {
     .map((item) => `${item.substr(1)}${item[0]}ay`)
     .join(" ");
 }
+// Question15 | ky8 | Student's Final Grade
+
+/*Create a function finalGrade, which calculates the final grade of a student depending on two parameters: a grade for the exam and a number of completed projects.
+
+This function should take two arguments: exam - grade for exam (from 0 to 100); projects - number of completed projects (from 0 and above);
+
+This function should return a number (final grade). There are four types of final grades:
+
+100, if a grade for the exam is more than 90 or if a number of completed projects more than 10.
+90, if a grade for the exam is more than 75 and if a number of completed projects is minimum 5.
+75, if a grade for the exam is more than 50 and if a number of completed projects is minimum 2.
+0, in other cases
+Examples(Inputs-->Output):
+
+100, 12 --> 100
+99, 0 --> 100
+10, 15 --> 100
+
+85, 5 --> 90
+
+55, 3 --> 75
+
+55, 0 --> 0
+20, 2 --> 0*/
+
+function finalGrade(exam, projects) {
+  if (exam > 90 || projects > 10) return 100;
+  if ((exam > 75) & (projects >= 5)) return 90;
+  if ((exam > 50) & (projects >= 2)) return 75;
+  return 0;
+}
+// Question16 | ky8 | Grasshopper - Terminal game move function
+
+/*Terminal game move function
+In this game, the hero moves from left to right. The player rolls the dice and moves the number of spaces indicated by the dice two times.
+
+Create a function for the terminal game that takes the current position of the hero and the roll (1-6) and return the new position.
+
+Example:
+move(3, 6) should equal 15*/
+
+const move = (position, roll) => position + roll * 2;
