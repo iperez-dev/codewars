8391d38dcae8a29296fcadeb3261dbb5e06beaa3
Ivan Perez <ivan.web9450@gmail.com>
2023-07-25 09:00:08 -0400

    Question34 | ky8 | Double Char

--- JS/script.js
+++ JS/script.js
@@ -583,13 +583,18 @@ function firstNonConsecutive(arr) {
   }
   return null;
 }
-// Question34 | ky8 | Beginner Series #4 Cockroach
+// Question34 | ky8 | Double Char
 /*
-The cockroach is one of the fastest insects. Write a function which takes its speed in km per hour and returns it in cm per second, rounded down to the integer (= floored).
+Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
 
-For example:
-
-1.08 --> 30
-Note! The input is a Real number (actual type is language dependent) and is >= 0. The result should be an Integer.
+Examples (Input -> Output):
+* "String"      -> "SSttrriinngg"
+* "Hello World" -> "HHeelllloo  WWoorrlldd"
+* "1234!_ "     -> "11223344!!__  "
+Good Luck!
 */
-const cockroachSpeed = (s) => Math.floor(s / 0.036);
+const doubleChar = (str) =>
+  str
+    .split("")
+    .map((c) => c + c)
+    .join("");
