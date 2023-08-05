20e1214cbb5a13aabfb2c6f6629a8291713dd289
Ivan Perez <ivan.web9450@gmail.com>
2023-08-05 09:06:54 -0400

    Question45 | ky8 | Area or Perimeter

--- JS/script.js
+++ JS/script.js
@@ -751,7 +751,7 @@ function sumArray(array) {
 }
 
 /*
-// ccccccccc
+// Question43 | ky8 | Sum without highest and lowest number
 Grade book
 Complete the function so that it finds the average of the three scores passed to it and returns the letter value associated with that grade.
 
@@ -771,3 +771,35 @@ function getGrade(s1, s2, s3) {
   else if (avg < 90) return "B";
   else return "A";
 }
+
+/*
+Question44 | ky8 | Grasshopper - Grade book
+Grade book
+Complete the function so that it finds the average of the three scores passed to it and returns the letter value associated with that grade.
+
+Numerical Score	Letter Grade
+90 <= score <= 100	'A'
+80 <= score < 90	'B'
+70 <= score < 80	'C'
+60 <= score < 70	'D'
+0 <= score < 60	'F'
+Tested values are all between 0 and 100. Theres is no need to check for negative values or values greater than 100.
+*/
+function getGrade(s1, s2, s3) {
+  let s = (s1 + s2 + s3) / 3;
+  return s >= 90 ? "A" : s >= 80 ? "B" : s >= 70 ? "C" : s >= 60 ? "D" : "F";
+}
+/*
+Question45 | ky8 | Area or Perimeter
+You are given the length and width of a 4-sided polygon. The polygon can either be a rectangle or a square.
+If it is a square, return its area. If it is a rectangle, return its perimeter.
+
+Example(Input1, Input2 --> Output):
+
+6, 10 --> 32
+3, 3 --> 9
+Note: for the purposes of this kata you will assume that it is a square if its length and width are equal, otherwise it is a rectangle.
+*/
+const areaOrPerimeter = function (l, w) {
+  return l == w ? l * w : 2 * (l + w);
+};
