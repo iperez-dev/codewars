f36259176f8970afff95f09764689addd3f890a5
Ivan Perez <ivan.web9450@gmail.com>
2023-08-07 22:29:17 -0400

    Question52 | ky8 | Transportation on vacation

--- JS/script.js
+++ JS/script.js
@@ -904,3 +904,25 @@ function countSheep(num) {
 }
 
 console.log(countSheep(8));
+/*
+Question52 | ky8 | 
+After a hard quarter in the office you decide to get some rest on a vacation. So you will book a flight for you and your girlfriend and try to leave all the mess behind you.
+
+You will need a rental car in order for you to get around in your vacation. The manager of the car rental makes you some good offers.
+
+
+Every day you rent the car costs $40. If you rent the car for 7 or more days, you get $50 off your total. Alternatively, if you rent the car for 3 or more days, you get $20 off your total.
+
+Write a code that gives out the total amount for different days(d).
+*/
+// $40 day | 7 -$50 | 3-7 -$20
+//posible solutions ->  ternary operator
+
+function totalPrice(days) {
+  return days < 3 ? days * 40 : days < 7 ? days * 40 - 20 : days * 40 - 50;
+}
+
+//test-cases
+console.log(totalPrice(1), "$40");
+console.log(totalPrice(5), "$180");
+console.log(totalPrice(10), "$350");
