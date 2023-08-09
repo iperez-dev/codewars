ac6f796ba876add499b47b87f4442a6cc609129d
Ivan Perez <ivan.web9450@gmail.com>
2023-08-09 16:11:31 -0400

    Question59 | ky8 | How good are you really?

--- JS/script.js
+++ JS/script.js
@@ -1082,3 +1082,29 @@ console.log(getToPump(25), "true");
 console.log(getToPump(35), "true");
 console.log(getToPump(10), "false");
 console.log(getToPump(1), "false");
+/*
+Question59 | ky8 | How good are you really?
+There was a test in your class and you passed it. Congratulations!
+But you're an ambitious person. You want to know if you're better than the average student in your class.
+
+You receive an array with your peers' test scores. Now calculate the average and compare your score!
+
+Return True if you're better, else False!
+
+Note:
+Your points are not included in the array of your class's points. For calculating the average point you may add your point to the given array!
+
+
+*/
+//
+//
+//return = true or false
+//solutions = peersScores.reduce((acc, num) => (acc + num) / 3, 0) | ternary operator
+
+function betterThanAve(peersScores, yourScore) {
+  return peersScores.reduce((acc, num) => acc + num, 0) / peersScores.length;
+}
+
+//test-cases
+console.log(betterThanAve([67, 48, 90, 87, 70], 1), "false");
+console.log(betterThanAve([67, 48, 90, 87, 70], 100), "true");
