3d87c9798bfa8c6a0bb3d9be476360966202e830
Ivan Perez <ivan.web9450@gmail.com>
2023-09-05 08:51:17 -0400

    Question76 | ky8 | Basic data types--Number

--- JS/script.js
+++ JS/script.js
@@ -1507,3 +1507,19 @@ function positiveNums(n) {
 console.log(positiveNums(4), [1, 2, 3, 4]);
 console.log(positiveNums(1), [1]);
 console.log(positiveNums(5), [1, 2, 3, 4, 5]);
+/*
+Question76 | ky8 | Basic data types--Number
+
+I've written five functions equal1,equal2,equal3,equal4,equal5, defines six global variables v1 v2 v3 v4 v5 v6, every function has two local variables a,b, please set the appropriate value for the two variables(select from v1--v6), making these function return value equal to 100. the function equal1 is completed, please refer to this example to complete the following functions.
+
+*/
+//return =
+//soluton =
+var v1 = 50;
+(v2 = 100), (v3 = 150), (v4 = 200), (v5 = 2), (v6 = 250);
+
+const equal1 = () => v1 + v1;
+const equal2 = () => v3 - v1;
+const equal3 = () => v1 * v5;
+const equal4 = () => v4 / v5;
+const equal5 = () => v2 % v4;
