15e410e8b931f380090b9c7b1d188b529acd30b1
Ivan Perez <ivan.web9450@gmail.com>
2023-09-13 09:39:17 -0400

    Question83 | ky8 | Is this my tail?

--- JS/script.js
+++ JS/script.js
@@ -1646,3 +1646,25 @@ function addFive(num) {
 console.log(addFive(5), 10);
 console.log(addFive(2), 7);
 console.log(addFive(0), 5);
+/*
+Question83 | ky8 | Is this my tail?
+
+Some new animals have arrived at the zoo. The zoo keeper is concerned that perhaps the animals do not have the right tails. To help her, you must correct the broken function to make sure that the second argument (tail), is the same as the last letter of the first argument (body) - otherwise the tail wouldn't fit!
+
+If the tail is right return true, else return false.
+
+The arguments will always be non empty strings, and normal letters.
+
+*/
+//return = boolean
+//solution = body.charAt(body.length - 1) === tail
+
+function animalTails(body, tail) {
+  return body.charAt(body.length - 1) === tail ? true : false;
+}
+
+//test-cases
+console.log(animalTails("leon", "n"), true);
+console.log(animalTails("zebra", "n"), false);
+console.log(animalTails("zebra", "a"), true);
+console.log(animalTails("monkey", "a"), false);
