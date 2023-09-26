4aef9fe8f7ad2a16b0ea7195ff73b1a70c8fe707
Ivan Perez <ivan.web9450@gmail.com>
2023-09-26 15:05:28 -0400

    Question95 | ky8 | Basic Mathematical Operations

--- JS/script.js
+++ JS/script.js
@@ -1967,3 +1967,36 @@ function booleanToString(boolean) {
 //test-cases
 console.log(booleanToString(true), "true");
 console.log(booleanToString(false), "false");
+/*
+Question95 | ky8 | Basic Mathematical Operations
+
+Your task is to create a function that does four basic mathematical operations.
+
+The function should take three arguments - operation(string/char), value1(number), value2(number).
+The function should return result of numbers after applying the chosen operation.
+
+Examples(Operator, value1, value2) --> output
+('+', 4, 7) --> 11
+('-', 15, 18) --> -3
+('*', 5, 5) --> 25
+('/', 49, 7) --> 7
+
+*/
+//return = num
+//solution = if / else
+
+function mathOperations(operator, value1, value2) {
+  return operator === "+"
+    ? value1 + value2
+    : operator === "-"
+    ? value1 - value2
+    : operator === "*"
+    ? value1 * value2
+    : value1 / value2;
+}
+
+//test-cases
+console.log(mathOperations("+", 4, 7), 11);
+console.log(mathOperations("-", 15, 18), -3);
+console.log(mathOperations("*", 5, 5), 25);
+console.log(mathOperations("/", 49, 7), 7);
