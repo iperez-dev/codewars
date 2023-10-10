d9f80bd2776ef7a9a9ce4574b3eb1ce714be347b
Ivan Perez <ivan.web9450@gmail.com>
2023-10-10 07:01:49 -0400

    Question107 | ky7 | You're a square!

--- JS/script.js
+++ JS/script.js
@@ -2317,3 +2317,27 @@ console.log(getMiddle("test"), "es");
 console.log(getMiddle("testing"), "t");
 console.log(getMiddle("middle"), "dd");
 console.log(getMiddle("A"), "A");
+/*
+Question107 | ky7 | You're a square!
+
+In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+
+Example
+filter_list([1,2,'a','b']) == [1,2]
+filter_list([1,'a','b',0,15]) == [1,0,15]
+filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
+
+*/
+
+//pseudo-code = list num and strings -> filter -> return num
+//return = num
+//solution = arr.filter((num) => num)
+
+function filterStr(arr) {
+  return arr.filter((elem) => typeof elem === "number");
+}
+
+//test-cases
+console.log(filterStr([1, 2, "a", "b"]), [1, 2]);
+console.log(filterStr([1, "a", "b", 0, 15]), [1, 0, 15]);
+console.log(filterStr([1, 2, "aasf", "1", "123", 123]), [1, 2, 123]);
