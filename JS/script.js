767cf8f15d52a1f175995147dc08dbd5bd08254c
Ivan Perez <ivan.web9450@gmail.com>
2023-10-15 10:39:20 -0400

    Question103 | ky7 | Square Every Digit

--- JS/script.js
+++ JS/script.js
@@ -2190,3 +2190,35 @@ console.log(
   removeVowels("This website is for losers LOL!"),
   `Ths wbst s fr lsrs LL!`
 );
+/*
+Question103 | ky7 | Square Every Digit
+
+
+Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
+
+For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1. (81-1-1-81)
+
+Example #2: An input of 765 will/should return 493625 because 72 is 49, 62 is 36, and 52 is 25. (49-36-25)
+
+Note: The function accepts an integer and returns an integer.
+
+Happy Coding!
+
+*/
+
+//return = num
+//solution = .split("").map((elem) => elem * elem).join("")
+//question? Can I split a number, or I need to converted to a string first?
+
+function squareDigit(num) {
+  const str = String(num)
+    .split("")
+    .map((elem) => Number(elem * elem))
+    .join("");
+  return (num = Number(str));
+}
+
+//test-cases
+console.log(squareDigit(9119), 811181);
+console.log(squareDigit(765), 493625);
+console.log(squareDigit(234), 4916);
