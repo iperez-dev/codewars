849c8adc63133d47b4000f20f3d7fab352dccd65
Ivan Perez <ivan.web9450@gmail.com>
2023-11-09 09:08:09 -0500

    Question123 | ky7 | Number of People in the Bus

--- JS/script.js
+++ JS/script.js
@@ -2793,3 +2793,31 @@ function equalEnding(str1, str2) {
 console.log(equalEnding("abc", "bc"), true);
 console.log(equalEnding("abc", "d"), false);
 console.log(equalEnding("hello world", "d"), true);
+/*
+Question122 | ky7 | String ends with?
+
+Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+Examples:
+
+solution('abc', 'bc') // returns true
+solution('abc', 'd') // returns false
+*/
+function solution(str, ending) {
+  return str.endsWith(ending);
+}
+/*
+Question123 | ky7 | Number of People in the Bus
+There is a bus moving in the city which takes and drops some people at each bus stop.
+
+You are provided with a list (or array) of integer pairs. Elements of each pair represent the number of people that get on the bus (the first item) and the number of people that get off the bus (the second item) at a bus stop.
+
+Your task is to return the number of people who are still on the bus after the last bus stop (after the last array). Even though it is the last bus stop, the bus might not be empty and some people might still be inside the bus, they are probably sleeping there :D
+
+Take a look on the test cases.
+
+Please keep in mind that the test cases ensure that the number of people in the bus is always >= 0. So the returned integer can't be negative.
+
+The second value in the first pair in the array is 0, since the bus is empty in the first bus stop.
+
+*/
