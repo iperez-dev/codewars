bf5d2a525df16fa65dc074fded81cfc8458a1732
Ivan Perez <ivan.web9450@gmail.com>
2023-11-11 08:35:15 -0500

    Question126 | ky7 | Find the divisors!

--- JS/script.js
+++ JS/script.js
@@ -2933,3 +2933,34 @@ function reverseWords(str) {
 console.log(reverseWords("Hello world"));
 console.log(reverseWords("a b c d"));
 console.log(reverseWords("double  spaced  words"));
+/*
+Question126 | ky7 | Find the divisors!
+
+Create a function named divisors/Divisors that takes an integer n > 1 and returns an array with all of the integer's divisors(except for 1 and the number itself), from smallest to largest. If the number is prime return the string '(integer) is prime' (null in C#, empty table in COBOL) (use Either String a in Haskell and Result<Vec<u32>, String> in Rust).
+
+Example:
+divisors(12); // should return [2,3,4,6]
+divisors(25); // should return [5]
+divisors(13); // should return "13 is prime"
+
+*/
+function divisors(n) {
+  let divisors = [];
+
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) {
+      divisors.push(i);
+      if (i !== n / i) {
+        divisors.push(n / i);
+      }
+    }
+  }
+
+  divisors.sort((a, b) => a - b);
+
+  return divisors.length === 0 ? `${n} is prime` : divisors;
+}
+
+console.log(divisors(12));
+console.log(divisors(25));
+console.log(divisors(13));
