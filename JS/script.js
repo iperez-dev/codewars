32736be20c9f41afed6bb70fc9a7bb9e31847f6a
Ivan Perez <ivan.web9450@gmail.com>
2023-11-12 08:37:35 -0500

    Question127 | ky7 | Sum of the first nth term of Series

--- JS/script.js
+++ JS/script.js
@@ -2964,3 +2964,36 @@ function divisors(n) {
 console.log(divisors(12));
 console.log(divisors(25));
 console.log(divisors(13));
+/*
+Question127 | ky7 | Sum of the first nth term of Series
+
+Task:
+Your task is to write a function which returns the sum of following series upto nth term(parameter).
+
+Series: 1 + 1/4 + 1/7 + 1/10 + 1/13 + 1/16 +...
+Rules:
+You need to round the answer to 2 decimal places and return it as String.
+
+If the given value is 0 then it should return 0.00
+
+You will only be given Natural Numbers as arguments.
+
+Examples:(Input --> Output)
+1 --> 1 --> "1.00"
+2 --> 1 + 1/4 --> "1.25"
+5 --> 1 + 1/4 + 1/7 + 1/10 + 1/13 --> "1.57"
+
+*/
+function seriesSum(n) {
+  let sum = 0;
+
+  for (let i = 0; i < n; i++) {
+    sum += 1 / (1 + 3 * i);
+  }
+
+  return sum.toFixed(2);
+}
+
+console.log(seriesSum(1)); // "1.00"
+console.log(seriesSum(2)); // "1.25"
+console.log(seriesSum(5)); // "1.57"
