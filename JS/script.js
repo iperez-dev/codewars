76b2dd776492e6bfd5b88f1c783d754f3fd77432
Ivan Perez <ivan.web9450@gmail.com>
2023-11-14 08:42:15 -0500

    Question129 | ky7 | Testing 1-2-3

--- JS/script.js
+++ JS/script.js
@@ -3035,3 +3035,25 @@ function removeSmallest(numbers) {
 console.log(removeSmallest([1, 2, 3, 4, 5])); // Output: [2, 3, 4, 5]
 console.log(removeSmallest([5, 3, 2, 1, 4])); // Output: [5, 3, 2, 4]
 console.log(removeSmallest([2, 2, 1, 2, 1])); // Output: [2, 2, 2, 1]
+/*
+Question129 | ky7 | Testing 1-2-3
+
+Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+
+Write a function which takes a list of strings and returns each line prepended by the correct number.
+
+The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+
+Examples: (Input --> Output)
+
+[] --> []
+["a", "b", "c"] --> ["1: a", "2: b", "3: c"]
+
+*/
+
+function numberLines(lines) {
+  return lines.map((line, index) => `${index + 1}: ${line}`);
+}
+
+console.log(numberLines([])); // Output: []
+console.log(numberLines(["a", "b", "c"])); // Output: ["1: a", "2: b", "3: c"]
