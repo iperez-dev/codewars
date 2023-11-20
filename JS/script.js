0c22b1ff707215ddf64f894aacc9d622ab2ece4c
Ivan Perez <ivan.web9450@gmail.com>
2023-11-20 08:51:30 -0500

    Question133 | ky7 | Count the divisors of a number

--- JS/script.js
+++ JS/script.js
@@ -3144,3 +3144,40 @@ function findUniqueNumber(numbers) {
 // Example usage
 console.log(findUniqueNumber([1, 1, 2])); // Output: 2
 console.log(findUniqueNumber([17, 17, 3, 17, 17, 17, 17])); // Output: 3
+
+/*
+Question133 | ky7 | Count the divisors of a number
+
+Count the number of divisors of a positive integer n.
+
+Random tests go up to n = 500000.
+
+Examples (input --> output)
+4 --> 3 // we have 3 divisors - 1, 2 and 4
+5 --> 2 // we have 2 divisors - 1 and 5
+12 --> 6 // we have 6 divisors - 1, 2, 3, 4, 6 and 12
+30 --> 8 // we have 8 divisors - 1, 2, 3, 5, 6, 10, 15 and 30
+Note you should only return a number, the count of divisors. The numbers between parentheses are shown only for you to see which numbers are counted in each case.
+
+*/
+function countDivisors(n) {
+  let count = 0;
+
+  for (let i = 1; i <= Math.sqrt(n); i++) {
+    if (n % i == 0) {
+      if (n / i == i) {
+        count++;
+      } else {
+        count += 2;
+      }
+    }
+  }
+
+  return count;
+}
+
+// Example usage:
+console.log(countDivisors(4)); // Output: 3
+console.log(countDivisors(5)); // Output: 2
+console.log(countDivisors(12)); // Output: 6
+console.log(countDivisors(30)); // Output: 8
