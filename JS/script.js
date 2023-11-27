36661adbb10aca3aae49e65868e1b2b88376ede9
Ivan Perez <ivan.web9450@gmail.com>
2023-11-27 09:03:47 -0500

    Question139 | ky7 | Make a function that does arithmetic!

--- JS/script.js
+++ JS/script.js
@@ -3333,3 +3333,39 @@ function countDigitInSquares(n, d) {
 // Example usage:
 console.log(countDigitInSquares(10, 1)); // should return 4
 console.log(countDigitInSquares(25, 1)); // should return 11
+
+/*
+Question139 | ky7 | Make a function that does arithmetic!
+
+Given two numbers and an arithmetic operator (the name of it, as a string), return the result of the two numbers having that operator used on them.
+
+a and b will both be positive integers, and a will always be the first number in the operation, and b always the second.
+
+The four operators are "add", "subtract", "divide", "multiply".
+
+A few examples:(Input1, Input2, Input3 --> Output)
+
+5, 2, "add"      --> 7
+5, 2, "subtract" --> 3
+5, 2, "multiply" --> 10
+5, 2, "divide"   --> 2.5
+Try to do it without using if statements!
+
+*/
+
+function calculate(a, b, operator) {
+  const operations = {
+    add: (x, y) => x + y,
+    subtract: (x, y) => x - y,
+    multiply: (x, y) => x * y,
+    divide: (x, y) => x / y,
+  };
+
+  return operations[operator](a, b);
+}
+
+// Example usage:
+console.log(calculate(5, 2, "add")); // --> 7
+console.log(calculate(5, 2, "subtract")); // --> 3
+console.log(calculate(5, 2, "multiply")); // --> 10
+console.log(calculate(5, 2, "divide")); // --> 2.5
