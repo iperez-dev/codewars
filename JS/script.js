103b3c90b6c024b7d73bc429492fe3965873ae56
Ivan Perez <ivan.web9450@gmail.com>
2023-11-29 09:06:29 -0500

    Question140 | ky7 | Find the middle element

--- JS/script.js
+++ JS/script.js
@@ -3369,3 +3369,34 @@ console.log(calculate(5, 2, "add")); // --> 7
 console.log(calculate(5, 2, "subtract")); // --> 3
 console.log(calculate(5, 2, "multiply")); // --> 10
 console.log(calculate(5, 2, "divide")); // --> 2.5
+
+/*
+Question140 | ky7 | Find the middle element
+
+As a part of this Kata, you need to create a function that when provided with a triplet, returns the index of the numerical element that lies between the other two elements.
+
+The input to the function will be an array of three distinct numbers (Haskell: a tuple).
+
+For example:
+
+gimme([2, 3, 1]) => 0
+2 is the number that fits between 1 and 3 and the index of 2 in the input array is 0.
+
+Another example (just to make sure it is clear):
+
+gimme([5, 10, 14]) => 1
+10 is the number that fits between 5 and 14 and the index of 10 in the input array is 1.
+
+*/
+
+function gimme(triplet) {
+  const sortedTriplet = [...triplet].sort((a, b) => a - b);
+
+  const middle = sortedTriplet[1];
+
+  return triplet.indexOf(middle);
+}
+
+// Example usage
+console.log(gimme([2, 3, 1])); // Output: 0
+console.log(gimme([5, 10, 14])); // Output: 1
