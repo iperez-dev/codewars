f06c0c9202fc57d3cf7bc14943c83b2929aafdc1
Ivan Perez <ivan.web9450@gmail.com>
2023-12-01 09:11:07 -0500

    Question142 | ky7 | Sum of a sequence

--- JS/script.js
+++ JS/script.js
@@ -3440,3 +3440,40 @@ console.log(roundToNext5(21)); // Output: 25
 console.log(roundToNext5(30)); // Output: 30
 console.log(roundToNext5(-2)); // Output: 0
 console.log(roundToNext5(-5)); // Output: -5
+
+/*
+Question142 | ky7 | Sum of a sequence
+
+Your task is to write a function which returns the sum of a sequence of integers.
+
+The sequence is defined by 3 non-negative values: begin, end, step.
+
+If begin value is greater than the end, your function should return 0. If end is not the result of an integer number of steps, then don't add it to the sum. See the 4th example below.
+
+Examples
+
+2,2,2 --> 2
+2,6,2 --> 12 (2 + 4 + 6)
+1,5,1 --> 15 (1 + 2 + 3 + 4 + 5)
+1,5,3  --> 5 (1 + 4)
+
+*/
+
+function sequenceSum(begin, end, step) {
+  if (begin > end) {
+    return 0;
+  }
+
+  let sum = 0;
+  for (let i = begin; i <= end; i += step) {
+    sum += i;
+  }
+
+  return sum;
+}
+
+// Example usage
+console.log(sequenceSum(2, 2, 2)); // Output: 2
+console.log(sequenceSum(2, 6, 2)); // Output: 12
+console.log(sequenceSum(1, 5, 1)); // Output: 15
+console.log(sequenceSum(1, 5, 3)); // Output: 5
