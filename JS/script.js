a33b647c6c2a8280e685df54ccdf2c2fc5c11e4a
Perez <ivan.perez@woolpert.com>
2024-02-18 15:45:33 -0500

    Question199 | ky7 | Multiples of 3 or 5

--- JS/script.js
+++ JS/script.js
@@ -5232,3 +5232,30 @@ function solve(s) {
 
 // Example usage
 console.log(solve("gh12cdy695m1")); // Output: 695
+/*
+Question199 | ky7 | Multiples of 3 or 5
+
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+
+Additionally, if the number is negative, return 0.
+
+Note: If the number is a multiple of both 3 and 5, only count it once.
+
+*/
+
+function sumOfMultiples(n) {
+  if (n < 0) {
+      return 0;
+  } else {
+      return Array.from({ length: n }, (_, i) => i)
+          .filter(num => num % 3 === 0 || num % 5 === 0)
+          .reduce((acc, val) => acc + val, 0);
+  }
+}
+
+// Example usage:
+console.log(sumOfMultiples(10)); // Output should be 23
+
+
