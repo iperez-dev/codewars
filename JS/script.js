1f87bdf480a27be47b651e3edd9cf48016d0876a
iperez-dev <ivan.web9450@gmail.com>
2024-03-07 15:52:01 -0500

    ky6 | Highest Scoring Word

--- JS/script.js
+++ JS/script.js
@@ -5727,6 +5727,50 @@ console.log(findMissingLetter(['a','b','c','d','f'])); // Output: 'e'
 console.log(findMissingLetter(['O','Q','R','S'])); // Output: 'P'
 
 
+/*
+ky6 | Highest Scoring Word
+
+Given a string of words, you need to find the highest scoring word.
+
+Each letter of a word scores points according to its position in the alphabet: a = 1, b = 2, c = 3 etc.
+
+For example, the score of abad is 8 (1 + 2 + 1 + 4).
+
+You need to return the highest scoring word as a string.
+
+If two words score the same, return the word that appears earliest in the original string.
+
+All letters will be lowercase and all inputs will be valid.
+
+*/
+
+function highestScoringWord(str) {
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  let maxScore = 0;
+  let highestWord = '';
+
+  const words = str.split(' ');
+  
+  for (let word of words) {
+    let score = 0;
+    for (let char of word) {
+      score += alphabet.indexOf(char) + 1;
+    }
+    if (score > maxScore) {
+      maxScore = score;
+      highestWord = word;
+    }
+  }
+
+  return highestWord;
+}
+
+// Test cases
+console.log(highestScoringWord("abad test")); // Output: 'abad'
+console.log(highestScoringWord("test abad")); // Output: 'test'
+
+
+
 
 
 
