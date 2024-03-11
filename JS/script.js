0c9a2e4e0199e020386c74135059bf98e7f27a2d
iperez-dev <ivan.web9450@gmail.com>
2024-03-11 16:49:27 -0400

    ky6 | Find the missing letter

--- JS/script.js
+++ JS/script.js
@@ -5690,7 +5690,43 @@ function sortArray(array) {
 
 // Test cases
 console.log(sortArray([7, 1])); // Output: [1, 7]
-console.log(sortArray([5, 
+
+
+/*
+ky6 | Find the missing letter
+
+Write a method that takes an array of consecutive (increasing) letters as input and that returns the missing letter in the array.
+
+You will always get an valid array. And it will be always exactly one letter be missing. The length of the array will always be at least 2.
+The array will always contain letters in only one case.
+
+Example:
+
+['a','b','c','d','f'] -> 'e'
+['O','Q','R','S'] -> 'P'
+(Use the English alphabet with 26 letters!)
+
+Have fun coding it and please don't forget to vote and rank this kata! :-)
+
+I have also created other katas. Take a look if you enjoyed this kata!
+
+*/
+
+function findMissingLetter(array) {
+  for (let i = 0; i < array.length - 1; i++) {
+    // Check if the next letter is not consecutive
+    if (array[i].charCodeAt(0) + 1 !== array[i + 1].charCodeAt(0)) {
+      // Return the missing letter
+      return String.fromCharCode(array[i].charCodeAt(0) + 1);
+    }
+  }
+}
+
+// Test cases
+console.log(findMissingLetter(['a','b','c','d','f'])); // Output: 'e'
+console.log(findMissingLetter(['O','Q','R','S'])); // Output: 'P'
+
+
 
 
 
