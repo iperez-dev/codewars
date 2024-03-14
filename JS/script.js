e063cbf0dfff146153e1342c86fdbc785f913311
iperez-dev <ivan.web9450@gmail.com>
2024-03-14 16:47:29 -0400

    ky6 | Sort the odd

--- JS/script.js
+++ JS/script.js
@@ -5664,6 +5664,36 @@ console.log(splitPairs('abc')); // Output: ['ab', 'c_']
 console.log(splitPairs('abcdef')); // Output: ['ab', 'cd', 'ef']
 
 
+/*
+ky6 | Sort the odd
+
+You will be given an array of numbers. You have to sort the odd numbers in ascending order while leaving the even numbers at their original positions.
+
+Examples
+[7, 1]  =>  [1, 7]
+[5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
+[9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
+
+*/
+
+function sortArray(array) {
+  const oddNumbers = array.filter(num => num % 2 !== 0).sort((a, b) => a - b);
+  let oddIndex = 0;
+  
+  return array.map(num => {
+    if (num % 2 !== 0) {
+      return oddNumbers[oddIndex++];
+    }
+    return num;
+  });
+}
+
+// Test cases
+console.log(sortArray([7, 1])); // Output: [1, 7]
+console.log(sortArray([5, 
+
+
+
 
 
 
