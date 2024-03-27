856ad175fd7f0c1cf8a91a6b6e91ff12031f83d0
iperez-dev <ivan.web9450@gmail.com>
2024-03-27 13:16:46 -0400

    Create Phone Number

--- JS/script.js
+++ JS/script.js
@@ -5382,5 +5382,34 @@ console.log(arrayDiff([1, 2, 2, 2, 3], [2]));    // Output: [1, 3]
 
 
 
+/*
+ky6 | Create Phone Number
+
+Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
+
+Example
+createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(123) 456-7890"
+The returned format must be correct in order to complete this challenge.
+
+Don't forget the space after the closing parentheses!
+
+*/
+
+function createPhoneNumber(numbers) {
+  // Extract each part of the phone number
+  const areaCode = numbers.slice(0, 3).join('');
+  const firstPart = numbers.slice(3, 6).join('');
+  const secondPart = numbers.slice(6, 10).join('');
+
+  // Return the formatted phone number string
+  return `(${areaCode}) ${firstPart}-${secondPart}`;
+}
+
+// Test case
+console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])); // Output: "(123) 456-7890"
+
+
+
+
 
 
