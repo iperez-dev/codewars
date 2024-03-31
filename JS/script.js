7ae600f448ed49fab2010a91e446a411037cfbe4
iperez-dev <ivan.web9450@gmail.com>
2024-03-31 16:46:49 -0400

    Encrypt this!

--- JS/script.js
+++ JS/script.js
@@ -6187,6 +6187,43 @@ function findMissing(arr) {
 // Example usage
 console.log(findMissing([1, 3, 5, 9, 11])); // Output: 7
 
+/*ky6 | Encrypt this!
+
+Encrypt this!
+
+You want to create secret messages which can be deciphered by the Decipher this! kata. Here are the conditions:
+
+Your message is a string containing space separated words.
+You need to encrypt each word in the message using the following rules:
+The first letter must be converted to its ASCII code.
+The second letter must be switched with the last letter
+Keepin' it simple: There are no special characters in the input.
+Examples:
+encryptThis("Hello") === "72olle"
+encryptThis("good") === "103doo"
+encryptThis("hello world") === "104olle 119drlo"
+
+*/
+
+function encryptThis(text) {
+  return text.split(' ').map(word => {
+      if (word.length === 1) {
+          return word.charCodeAt(0);
+      } else if (word.length === 2) {
+          return word.charCodeAt(0) + word[1];
+      } else {
+          return word.charCodeAt(0) + word[word.length - 1] + word.slice(2, -1) + word[1];
+      }
+  }).join(' ');
+}
+
+// Example usage
+console.log(encryptThis("Hello")); // Output: "72olle"
+console.log(encryptThis("good")); // Output: "103doo"
+console.log(encryptThis("hello world")); // Output: "104olle 119drlo"
+
+
+
 
 
 
