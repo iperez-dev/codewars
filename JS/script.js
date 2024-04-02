e6853e6ba52fb1a3a9dc223ec318a01cf22ecb69
Ivan <ivan.perez@woolpert.com>
2024-04-02 11:48:50 -0400

    test2

--- JS/script.js
+++ JS/script.js
@@ -5258,7 +5258,7 @@ function sumOfMultiples(n) {
 // Example usage:
 console.log(sumOfMultiples(10)); // Output should be 23
 /*
-Question200 | ky6 | Stop gninnipS My sdroW!
+Question200 | ky6 | Stop gninnipS My sdroW!!!!!!!!
 
 Write a function that takes in a string of one or more words, and returns the same string, but with all words that have five or more letters reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
 
