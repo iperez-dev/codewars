1351149a92a79d7b41f991db123db12be0c685a3
iperez-dev <ivan.web9450@gmail.com>
2024-04-06 16:35:53 -0400

    ky6 | Your order, please

--- JS/script.js
+++ JS/script.js
@@ -5538,6 +5538,35 @@ function isNarcissisticNumber(number) {
 // Test cases
 console.log(isNarcissisticNumber(153)); // Output: true
 console.log(isNarcissisticNumber(1652)); // Output: false
+/*
+ky6 | Your order, please
+
+Your task is to sort a given string. Each word in the string will contain a single number. This number is the position the word should have in the result.
+
+Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+
+If the input string is empty, return an empty string. The words in the input String will only contain valid consecutive numbers.
+
+Examples
+"is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
+"4of Fo1r pe6ople g3ood th5e the2"  -->  "Fo1r the2 g3ood 4of th5e pe6ople"
+""  -->  ""
+
+*/
+
+function order(words) {
+  if (words === '') return '';
+
+  return words.split(' ')
+              .sort((a, b) => a.match(/\d/) - b.match(/\d/))
+              .join(' ');
+}
+
+// Test cases
+console.log(order("is2 Thi1s T4est 3a")); // Output: "Thi1s is2 3a T4est"
+console.log(order("4of Fo1r pe6ople g3ood th5e the2")); // Output: "Fo1r the2 g3ood 4of th5e pe6ople"
+console.log(order("")); // Output: ""
+
 
 
 
