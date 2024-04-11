ef76a3ccb04eb6b182163264ed1a165a985b13ea
iperez-dev <ivan.web9450@gmail.com>
2024-04-11 16:54:47 -0400

    Consonant value

--- JS/script.js
+++ JS/script.js
@@ -6258,10 +6258,47 @@ function meeting(s) {
       .join('');
 }
 
+
+
+
+/*ky6 | Consonant value
+
+Given a lowercase string that has alphabetic characters only and no spaces, return the highest value of consonant substrings. Consonants are any letters of the alphabet except "aeiou".
+
+We shall assign the following values: a = 1, b = 2, c = 3, .... z = 26.
+
+For example, for the word "zodiacs", let's cross out the vowels. We get: "z o d ia cs"
+
+-- The consonant substrings are: "z", "d" and "cs" and the values are z = 26, d = 4 and cs = 3 + 19 = 22. The highest is 26.
+solve("zodiacs") = 26
+
+For the word "strength", solve("strength") = 57
+-- The consonant substrings are: "str" and "ngth" with values "str" = 19 + 20 + 18 = 57 and "ngth" = 14 + 7 + 20 + 8 = 49. The highest is 57.
+For C: do not mutate input.
+
+More examples in test cases. Good luck!
+
+*/
+
+function solve(s) {
+  // Function to calculate the value of a substring
+  const getValue = (substring) => {
+      return Array.from(substring).reduce((acc, char) => acc + (char.charCodeAt(0) - 96), 0);
+  };
+  
+  // Split the string on vowels to get consonant substrings
+  const consonantSubstrings = s.split(/[aeiou]/);
+  
+  // Calculate the value for each substring and find the maximum value
+  const maxValue = Math.max(...consonantSubstrings.map(getValue));
+  
+  return maxValue;
+}
+
 // Example usage
-const s = "Fred:Corwill;Wilfred:Corwill;Barney:Tornbull;Betty:Tornbull;Bjon:Tornbull;Raphael:Corwill;Alfred:Corwill";
-console.log(meeting(s)); 
-// Output: "(CORWILL, ALFRED)(CORWILL, FRED)(CORWILL, RAPHAEL)(CORWILL, WILFRED)(TORNBULL, BARNEY)(TORNBULL, BETTY)(TORNBULL, BJON)"
+console.log(solve("zodiacs")); // Output: 26
+console.log(solve("strength")); // Output: 57
+
 
 
 
